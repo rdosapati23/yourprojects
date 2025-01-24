596e3b2ab5c6c574dab52688136b4b46a9994231
Rakesh Dosapati <rdosapati23@gmail.com>
2025-01-24 12:20:12 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,14 @@
+package MyProjecttt;
+
+public class Pattern1 {
+	
+	public static void main(String[] args) {
+		for(int r=1;r<5;r++) {
+			for(int c=5,p=1;p<=r;c--,p++) {
+				System.out.print(c+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
