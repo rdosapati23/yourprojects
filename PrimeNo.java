4abc2b9b2eef5fd0835c5ec242759a04050503d0
Rakesh Dosapati <rdosapati23@gmail.com>
2025-01-27 12:21:35 +0530

    Add files via upload

---
+++ PrimeNo.java
@@ -0,0 +1,13 @@
+package com.rakesh.prime;
+
+public class PrimeNo {
+	public static boolean isPrime(int num) {
+		int count=0;
+		for(int i=1;i<=num;i++) {
+			if(num % i == 0) count++;
+		}
+		if(count == 2) return true;
+		else return false;
+	}
+
+}
